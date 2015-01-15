bf23356225890395bf31872274c789795f1d9ab1
Tom <absentisinaventulus@gmail.com>
2015-01-15 13:48:51 -0600

    Added Entity.cs

---
+++ Morinda/Entity.cs
@@ -0,0 +1,40 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+namespace Morinda
+{
+    /// <summary>
+    /// Entity Class:
+    ///     Encapsulates a GUID
+    /// </summary>
+    class Entity
+    {
+        // Encapsulated ID
+        public Guid id;
+
+        /// <summary>
+        /// Generates an Entity based on a givenGuid
+        /// </summary>
+        /// <param name="givenGuid"></param>
+        public Entity(Guid givenGuid)
+        {
+            id = givenGuid;
+        }
+
+        /// <summary>
+        /// Generates an Entity by grabbing a new GUID
+        /// </summary>
+        public Entity()
+        {
+            id = Guid.NewGuid();
+        }
+    }
+}
