2e227e693af24fcd7843338148ca0605538f5100
Tom <absentisinaventulus@gmail.com>
2015-01-20 15:12:21 -0600

    Added base workings for control system

---
+++ Morinda/Components/TransformComponent.cs
@@ -0,0 +1,27 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+namespace Morinda
+{
+    class TransformComponent : Component
+    {
+        float rotation;
+        float scale;
+        Vector2 position;
+
+        public TransformComponent(Vector2 givenPos, float givenScale, float givenRotation)
+        {
+            position = givenPos;
+            scale = givenScale;
+            rotation = givenRotation;
+        }
+    }
+}
---
+++ Morinda/Systems/ControlSystem.cs
@@ -0,0 +1,29 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+
+namespace Morinda.Systems
+{
+    class ControlSystem : System
+    {
+        Input input;
+
+        public ControlSystem(EntityManager givenManager) : base(givenManager)
+        {
+           
+        }
+
+        public void updateState(KeyboardState keyState, MouseState mouseState)
+        {
+
+        }
+    }
+}
